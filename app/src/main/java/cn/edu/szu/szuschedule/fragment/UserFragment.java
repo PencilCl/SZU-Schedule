e1fd71a8a50f42ca3167f7fd7f255d6beedf4ff7
Tongwenyang <397147004@qq.com>
2017-06-07 00:11:03 +0800

    修改了些登录界面跟用户界面功能

--- app/src/main/java/cn/edu/szu/szuschedule/fragment/UserFragment.java
+++ app/src/main/java/cn/edu/szu/szuschedule/fragment/UserFragment.java
@@ -1,22 +1,49 @@
 package cn.edu.szu.szuschedule.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import cn.edu.szu.szuschedule.LoginActivity;
 import cn.edu.szu.szuschedule.R;
+import cn.edu.szu.szuschedule.userTellUsActivity;
+
+import android.widget.Button;
+import android.widget.Toast;
 
 /**
  * Created by chenlin on 24/05/2017.
  */
-public class UserFragment extends Fragment {
+public class UserFragment extends Fragment{
+    View view;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_user, null);
-
+        view = inflater.inflate(R.layout.fragment_user, null);
+        Button loginOut = (Button)view.findViewById(R.id.loginOut);
+        loginOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getActivity().startActivity(new Intent(getActivity(),LoginActivity.class));
+                getActivity().finish();
+            }
+        });
+        Button aboutUs =  (Button)view.findViewById(R.id.aboutUs);
+        aboutUs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getActivity().startActivity(new Intent(getContext(),userTellUsActivity.class));
+            }
+        });
         return view;
     }
+
+
 }
