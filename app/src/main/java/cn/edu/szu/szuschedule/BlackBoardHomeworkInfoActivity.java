072b5966f3d839379e60ac9046eb9ccfbb4733a7
Pencil <9807175@qq.com>
2017-06-06 17:40:58 +0800

    Added: BB作业详情页面

---
+++ app/src/main/java/cn/edu/szu/szuschedule/BlackBoardHomeworkInfoActivity.java
@@ -0,0 +1,16 @@
+package cn.edu.szu.szuschedule;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by chenlin on 06/06/2017.
+ */
+public class BlackBoardHomeworkInfoActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bb_homework_info);
+    }
+}
