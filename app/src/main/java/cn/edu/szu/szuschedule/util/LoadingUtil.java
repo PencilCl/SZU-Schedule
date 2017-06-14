0cea9a623f23878da41b3bc29b136f979de4f441
Pencil <9807175@qq.com>
2017-06-14 21:41:43 +0800

    Added: LoadingUtil

--- app/src/main/java/cn/edu/szu/szuschedule/util/LoadingUtil.java
+++ app/src/main/java/cn/edu/szu/szuschedule/util/LoadingUtil.java
@@ -1,7 +1,47 @@
 package cn.edu.szu.szuschedule.util;
 
+import android.app.Dialog;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import cn.edu.szu.szuschedule.R;
+
 /**
  * Created by chenlin on 14/06/2017.
+ * 加载界面统一管理工具
  */
 public class LoadingUtil {
+    private Dialog mDialog;
+    private Integer count; // 计数当前需要显示Loading界面
+
+    public LoadingUtil(Context context) {
+        count = 0;
+
+        // 初始化Dialog
+        View mView = LayoutInflater.from(context)
+                .inflate(R.layout.dialog_loading, null);
+        mDialog = new Dialog(context, R.style.LoadingDialogStyle);
+        mDialog.setContentView(mView);
+        mDialog.setCanceledOnTouchOutside(false);
+    }
+
+    public void showLoading() {
+        synchronized (count) {
+            ++count;
+            if (count == 1) {
+                mDialog.show();
+            }
+        }
+    }
+
+    public void hideLoading() {
+        synchronized (count) {
+            if (count > 0) {
+                --count;
+                if (count == 0 && mDialog != null) {
+                    mDialog.dismiss();
+                }
+            }
+        }
+    }
 }
